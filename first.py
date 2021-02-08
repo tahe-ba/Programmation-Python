ac914a4a3e4a990dc5898af9da3723c6f5360588
tahe-ba <taha.ben.a@outlook.com>
2021-02-08 15:26:46 +0100

    first push

---
+++ first.py
@@ -0,0 +1,25 @@
+quitter = 'F'
+while quitter != 'T' :
+    while True :
+        try :
+            prix=float(input("prix HT="))
+            break
+        except ValueError:
+            print("Wrong input")
+
+    while True :
+        try :
+            option=int(input("if tva = 15% press 1 or press 2 if tva is 19% : "))
+            if option==1 :
+                tva=0.15
+                break
+            elif option == 2 :
+                tva =0.19
+                break
+            else :
+                print("only 1 or 2 are acceptable")
+        except ValueError:
+            print("Wrong input")
+    prixttc=prix+(tva*prix)
+    print(prixttc)
+    quitter=input("if you want tou quit type T: ")
