cdb911bdf92ec4a5ce3f476366457fa713be0ab2
tahe-ba <taha.ben.a@outlook.com>
2021-02-11 14:51:19 +0100

    ex8

--- serie/serie 1/code/ex8-custom_output.py
+++ serie/serie 1/code/ex8-custom_output.py
@@ -1,4 +1,14 @@
+#Function that takes x and returns x+2 and x**3
 def function(x):
     x=x+2
     return (x,x**3)
-print(function(2))
+
+while True :
+    try :
+        n=float(input("Donner un nombre n: "))
+        break
+    except ValueError:
+        print("Wrong input")
+
+a,b=function(n)
+print("n vaut : "+str(format(a, ".2f"))+" et n^3 vaut "+str(format(b, ".3f")))
