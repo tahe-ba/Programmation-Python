ddee9b2f9915d1b5b051f9585f94e146d00bf2b4
tahe-ba <taha.ben.a@outlook.com>
2021-02-25 11:13:09 +0100

    function

--- serie/serie 1/code/ex6-testThis.py
+++ serie/serie 1/code/ex6-testThis.py
@@ -1,6 +1,9 @@
 #Asked to run this function
+#accept many arguments in function 
+#return their sum
 def function(*nbre):
     resu=0
     for x in nbre:
-        resu+=x
+        resu += x
     print(resu)
+function(1,2,3) 
