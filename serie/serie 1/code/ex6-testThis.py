378a6e71ebd778554951fcb03c9603a7d1d14b59
tahe-ba <taha.ben.a@outlook.com>
2021-02-25 11:14:20 +0100

    type tuple

--- serie/serie 1/code/ex6-testThis.py
+++ serie/serie 1/code/ex6-testThis.py
@@ -1,6 +1,7 @@
 #Asked to run this function
 #accept many arguments in function 
 #return their sum
+#nbre inside the function behaves as a tuple
 def function(*nbre):
     resu=0
     for x in nbre:
