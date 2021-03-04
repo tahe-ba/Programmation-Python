b718a722f496862fde99822112be798371038743
tahe-ba <taha.ben.a@outlook.com>
2021-03-04 12:37:55 +0100

    -1 me

--- serie/serie 2/code/project.py
+++ serie/serie 2/code/project.py
@@ -1,6 +1,19 @@
 from random import randrange
 from math import ceil
 
+def color (n):
+    if n%2 == 0 :
+        couleur="noir"
+    else :
+        couleur="rouge"
+    return couleur
+
+def same_color(n,m):
+    if (n%2==0 and m%2==0 ) or (n%2!=0 and m%2!=0 ):
+        return True
+    else :
+        return False
+
 i=1
 wallet = 1000 
 continuer_partie = True 
@@ -9,7 +22,9 @@ while continuer_partie :
     print("\n####round(",i,")####")
     i+=1
     print("vous avez "+str(wallet)+" dans vos poche")
-    while True: 
+
+    bet = -1
+    while bet == -1: 
         try:  
             bet = int(input("\nChoisissez un nombre sur lequel vous voulez miser [0 .. 999] : "))
             if bet >= 0 and bet <= 999 :
@@ -17,11 +32,13 @@ while continuer_partie :
             else :
                 print("Nombre incorrect") 
         except ValueError: 
-            print("Aucun nombre saisie")   
+            print("Aucun nombre saisie")
+            bet=-1
             continue 
     #print (bet)
 
-    while True: 
+    mise = 0
+    while mise <= 0 or mise > wallet: 
         try: 
             mise = int(input("Tapez le montant de votre mise : "))
             if mise > wallet: 
@@ -33,22 +50,18 @@ while continuer_partie :
                 print("mise impossible")
         except ValueError: 
             print("Aucun nombre saisie")
+            mise=-1
             continue
     #print(mise)
 
     win = randrange(999)
-    print("\nLa roulette tourne et le numero gagnant est ", win,"\n")
-
-    if win%2 == 0 :
-        win_c="noir"
-    else :
-        win_c="rouge"
+    print("\nLa roulette tourne et le numero gagnant est", win,"de couleur",str(color(win)),"\n")
 
     if win == bet: 
         print("\nVous avez gagnez ", mise * 3)
         wallet += mise * 3
-    elif win % 2 == bet % 2:
-        print("Vous avez misé sur la couleur "+ win_c +" vous obtenez ", mise * 0.5)
+    elif same_color(win,bet):
+        print("Vous avez misé sur la couleur",str(color(bet)),"vous obtenez ", mise * 0.5)
         wallet += mise * 0.5
         wallet=ceil(wallet)
     else: 
@@ -57,7 +70,7 @@ while continuer_partie :
     #print(wallet)
 
     if wallet <= 0: 
-        print("poche vide vous avez perdu")
+        print("poche vide")
         continuer_partie = False
     else :
         quitter = input("il vous reste "+str(wallet)+"\n\nSi vous voulez quitter tapez o : ")
