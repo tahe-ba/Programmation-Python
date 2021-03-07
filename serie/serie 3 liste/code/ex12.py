d229bbacf2ad891f0532c91307c3bf5ad668360a
tahe-ba <taha.ben.a@outlook.com>
2021-03-07 15:58:15 +0100

    up

--- serie/serie 3 liste/code/ex12.py
+++ serie/serie 3 liste/code/ex12.py
@@ -2,17 +2,16 @@
 program to print the numbers of a specified list
 after removing evennumbers from it.
 '''
-li = [1,3,6,8,10,12]
-'''n=0
+li = [1,2,3,4,5,6,7,8,9]
 i=0
 m=len(li)
 while i<m:
     if (li[i]%2) == 0 :
-        li.pop(i-n)
-        n+=1
-    m=len(li)
+        li.pop(i)
+        m=len(li)
     i+=1
 print (li)
 '''
 li = [x for x in li if x%2!=0]
 print (li)
+'''
--- serie/serie 3 liste/code/ex13.py
+++ serie/serie 3 liste/code/ex13.py
@@ -1,3 +1,4 @@
+# program that shuffles a list 
 from random import shuffle
 li=[1,2,3,4,5,6]
 shuffle(li)
