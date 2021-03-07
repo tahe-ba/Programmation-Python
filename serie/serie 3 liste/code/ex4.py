3260c13070dabec67ecc6b2c1dea82072c616834
tahe-ba <taha.ben.a@outlook.com>
2021-03-07 11:53:06 +0100

    +

--- serie/serie 3 liste/code/ex4.py
+++ serie/serie 3 liste/code/ex4.py
@@ -8,7 +8,14 @@ Expected Result : 2
 li=["abc", "xyz", "aba", "1221"]
 o=0
 for i in range(len(li)):
-    a=list(li[i])
+    #a=list(li[i])
+    #list can be modified but tuple can't be modified
+    b=tuple(li[i])
+    if len(li[i]) >= 2 and b[0] == b[len(b)-1] :
+        o=o+1
+    '''
     if len(li[i]) >= 2 and a[0] == a[len(a)-1]: 
         o=o+1
+    '''
+
 print (o)
---
+++ serie/serie 3 liste/code/ex5.py
@@ -0,0 +1,6 @@
+'''
+Write a Python program to get a list, 
+sorted in increasing order by the last element ineach tuple from a given list of non-empty tuples.
+Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+'''
